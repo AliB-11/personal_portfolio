67bf066cdc2d069c39377a07e043e0a4c6c7724c
Ali Bandali <alimbandali@gmail.com>
2024-11-07 01:02:49 -0500

    tech stack for about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,14 @@
 import './index.scss'
 import MainNavBar from '../mainNavBar'
-import PageHeader from '../headers/pageHeader'
+import PageHeader from '../headers/pageHeader';
+import { FaNode, FaPython } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaNodeJs } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { SiCplusplus } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
 
 
 const About = () => {
@@ -12,6 +20,61 @@ const About = () => {
         <MainNavBar/>
          <div className="text-zone">
           <PageHeader>About</PageHeader>
+        </div>
+      <div className='skills-title'></div>
+      <div className='tech-stack-container'> 
+        <div className='languages'>
+            <div className='subtitle'>Languages</div> 
+            <div className='coding-chart'>
+               <div className='top-chart'>
+                  <div className='code'>
+                    <FaPython className='image' color='#ffd343'/>
+                    <div className='label'>Python</div>
+                  </div>
+                  <div className='code'>
+                    <FaJava className='image' color='#f89820'/>
+                    <div className='label'>Java</div>
+                  </div>
+                </div>
+                <div className='bottom-chart'>
+                  <div className='code'>
+                    <SiCplusplus className='image' color='#00549D'></SiCplusplus>
+                   <div className='label'>C/C++</div>
+                  </div>
+                  <div className='code'>
+                    <IoLogoJavascript className='image' color='	#f0db4f'/>
+                    <div className='label'>Javascript</div>
+                  </div>
+                </div>
+              </div>
+        </div>
+
+        <div className='languages frame'>
+            <div className='subtitle'>Framworks/Libraries</div> 
+            <div className='coding-chart'>
+               <div className='top-chart'>
+                  <div className='code'>
+                    <FaReact className='image' color='#61dbfb'/>
+                    <div className='label'>React</div>
+                  </div>
+                  <div className='code'>
+                    <FaNodeJs className='image' color='#66cc33'/>
+                    <div className='label'>Node</div>
+                  </div>
+                </div>
+                <div className='bottom-chart'>
+                  <div className='code'>
+                    <SiMongodb className='image mongo' color='#4db33d'/>
+                   <div className='label mongol'>MongoDB</div>
+                  </div>
+                  <div className='code'>
+                    <SiExpress className='image express' color='#ffffff'/>
+                    <div className='label'>Express</div>
+                  </div>
+                </div>
+              </div>
+        </div>
+
         </div>
       </div>
     </div>
--- src/components/particles.jsx
+++ src/components/particles.jsx
@@ -59,7 +59,7 @@ const ParticlesComponent = (props) => {
           }
         },
         "opacity": {
-          "value": 0.4,
+          "value": 0.6,
           "random": true,
           "anim": {
             "enable": true,
