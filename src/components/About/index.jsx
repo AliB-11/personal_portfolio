095e15e835c8019c84fda73746eb228319c3b072
Ali Bandali <alimbandali@gmail.com>
2024-11-07 22:02:30 -0500

    complete filling about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,6 +9,7 @@ import { FaReact } from "react-icons/fa";
 import { SiMongodb } from "react-icons/si";
 import { SiCplusplus } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
+import profileImage from '../../assets/images/profileImage.jpg'
 
 
 const About = () => {
@@ -21,7 +22,13 @@ const About = () => {
          <div className="text-zone">
           <PageHeader>About</PageHeader>
         </div>
-      <div className='skills-title'></div>
+      <div className='about-me'>
+        <div className='introduction'>
+          Hello! My name is Ali, and welcome to my portfolio. I am currently a second-year Computer Engineering student at McMaster University, with a strong interest in a variety of fields, including web development, machine learning, computer vision, and hardware engineering. Through this portfolio, I aim to showcase the projects and skills that reflect my academic journey, my dedication to teamwork, and my commitment to continuous improvement as an aspiring engineer. 
+        </div>
+      <img src={profileImage} className='profileImage'></img>
+      </div> 
+      <div className='skills-title'>Technical Skills</div>
       <div className='tech-stack-container'> 
         <div className='languages'>
             <div className='subcodetitle'>Languages</div> 
@@ -50,7 +57,7 @@ const About = () => {
         </div>
 
         <div className='languages frame'>
-            <div className='subcodetitle'>Framworks/Libraries</div> 
+            <div className='subcodetitle'>Frameworks/Libraries</div> 
             <div className='coding-chart'>
                <div className='top-chart'>
                   <div className='code'>
--- src/components/mainNavBar/index.jsx
+++ src/components/mainNavBar/index.jsx
@@ -6,9 +6,69 @@ import { LuUser2, LuMail} from "react-icons/lu";
 import { GoProject } from "react-icons/go";
 import { Link } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png'
+import { useState } from 'react';
+import { FiMenu } from "react-icons/fi";
+import { IoCloseOutline } from "react-icons/io5";
 
 const MainNavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const list = isOpen;
+
   return (
+    <> 
+    {list ? 
+      <div className='hamburger-background'>
+       <button className="close-icon" onClick={() =>  {setIsOpen(!isOpen)}}>
+          <IoCloseOutline color="white" size={30} />
+       </button>
+          <div className='wrappers'>
+          <nav> 
+            <ul className='navigation-button' > 
+              <li> 
+                <NavLink exact="true" activeclassname="active" to="/">
+                  <div className='nav-label'> 
+                    <BsHouse color='white' size={20} className='nav-icons'/> 
+                    <span className='headers'>Home</span>
+                  </div>
+                </NavLink>
+            </li>
+
+            <li>
+              <NavLink className="about-link" exact="true" activeclassname="active" to="/about">
+                <div className="nav-label">
+                  <LuUser2 color='white' size={20} className='nav-icons'/>
+                  <span className='headers'>About</span>
+                </div>
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink exact="true" activeclassname="active" to="/Projects">
+                <div className="nav-label">
+                  <GoProject  color='white' size={20} className='nav-icons'/>
+                  <span className='headers'>Projects</span>
+                </div>
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink className ="contact-link"exact="true" activeclassname="active" to="/contact" >
+                <div className="nav-label">
+                  <LuMail color='white' size={20} className='nav-icons'/>
+                  <span className='headers'>Contact</span>
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+          </nav>
+          </div>
+          </div>
+
+      : <button className="hamburger-icon" onClick={() =>  {setIsOpen(!isOpen)}}>
+          <FiMenu color="white" size={30} />
+      </button> }
+
     <div className='nav-bar-holder'>
       <div className='main-nav-bar'> </div> 
         <nav> 
@@ -54,6 +114,7 @@ const MainNavBar = () => {
         <img src={LogoS} className='logo-main' alt='logo'/>
      </Link>
     </div>
+    </>
   )
 }
 
