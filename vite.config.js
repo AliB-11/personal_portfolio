0cad05bb54e2ca9f667449317b6243f01f850e11
Ali <alimbandali@gmail.com>
2025-06-02 00:29:29 -0400

    fixing mobile UI

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/personal_portfolio/",
+  server: {
+    host: '0.0.0.0',  // ← this allows access from other devices on your network
+    port: 5173        // ← make sure to match this with the URL you use on your phone
+  }
 })
